c1166a6ca2edcf80b46c54772bdb8fe6b704aad6
Yuxuan Zhang <kate900115@gmail.com>
2018-12-30 10:48:46 +0000

    modified:   main.c

--- main.c
+++ main.c
@@ -96,7 +96,6 @@ void trainLayer(Layer *l){
 //		_mm256_store_pd(result, v4_input);
 //		printf("%f\n",result[1]);
 		__m256d v4_weight = _mm256_load_pd(weight+j);
-//		__m256d v4_weight = _mm256_set_pd(weight[j], weight[j+1], weight[j+2], weight[j+3]);
 		__m256d v4_mul = _mm256_mul_pd(v4_weight, v4_input);
 		v4_sum = _mm256_add_pd(v4_mul, v4_sum); 
     	    }
@@ -111,24 +110,25 @@ void trainLayer(Layer *l){
 	    __m256d v4_temp = _mm256_set1_pd(temp);
 
 	    double aaa[784];
-   
+   	  //  double* aaa;
+	   // aaa = (double*)malloc(784*sizeof(double));
 	    for (int j=0; j<NUMBER_OF_INPUT_CELLS; j=j+4){
 		__m256d v4_pixel = _mm256_set_pd(pixel[j+3], pixel[j+2], pixel[j+1], pixel[j+0]);
 		__m256d v4_input = _mm256_cmp_pd(v4_pixel, v4_zero, 4);
 		v4_input = _mm256_and_pd(v4_input, v4_one);
 		__m256d v4_mul = _mm256_mul_pd(v4_input, v4_temp);
 		__m256d v4_weight = _mm256_load_pd(weight+j);
-	//	__m256d v4_weight = _mm256_set_pd(weight[j], weight[j+1], weight[j+2], weight[j+3]);
 		v4_weight = _mm256_add_pd(v4_weight, v4_mul);
 		
-		_mm256_store_pd(aaa+j, v4_weight);
+		_mm256_storeu_pd(l->cell[i].weight+j, v4_weight);
+		//_mm256_store_pd(aaa+j, v4_weight);
 
 	//	printf("%f, %d\n",weight[j+3], j );
 	    }
 
-	    for (int j=0; j<784; j++){
+	  /*  for (int j=0; j<784; j++){
 		weight[j]=aaa[j];	
-	    }
+	    }*/
  /*
     	    #pragma simd
    	    for (int j=0; j<NUMBER_OF_INPUT_CELLS; j++){
@@ -216,7 +216,6 @@ void testLayer(Layer *l){
 		__m256d v4_pixel = _mm256_set_pd(pixel[j+3], pixel[j+2], pixel[j+1], pixel[j]);
 		__m256d v4_input = _mm256_cmp_pd(v4_pixel, v4_zero, 4);
 		v4_input = _mm256_and_pd(v4_input, v4_one);
-		//__m256d v4_weight = _mm256_set_pd(weight[j], weight[j+1], weight[j+2], weight[j+3]);
 		__m256d v4_weight = _mm256_load_pd(weight+j);
 		__m256d v4_mul = _mm256_mul_pd(v4_weight, v4_input);
 		v4_sum = _mm256_add_pd(v4_mul, v4_sum); 
