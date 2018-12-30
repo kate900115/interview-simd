b77b4e6429919404cf652a276bf7be39555c321e
Yuxuan Zhang <kate900115@gmail.com>
2018-12-30 08:33:10 +0000

    modified:   main.c

--- main.c
+++ main.c
@@ -163,18 +163,18 @@ void testLayer(Layer *l){
        // displayImage(&img, 8,6);
         
         // loop through all output cells for the given image
+	__m256d v4_zero = _mm256_setzero_pd();
+	__m256d v4_one = _mm256_set1_pd(1.0);
+
 	#pragma omp parallel for
         for (int i=0; i < NUMBER_OF_OUTPUT_CELLS; i++){
 	    double* input = l->cell[i].input; 
 	    double* weight = l->cell[i].weight;
-	    uint8_t* pixel = img.pixel; 
-	    double result[4];
-    	   // #pragma simd
- 	    __m256d v4_zero = _mm256_setzero_pd();
-	    __m256d v4_sum = _mm256_setzero_pd();
-	    __m256d v4_one = _mm256_set1_pd(1.0);
+	    uint8_t* pixel = img.pixel;
+ 	    __m256d v4_sum = _mm256_setzero_pd();
 
-    	    for (int j=0; j<NUMBER_OF_INPUT_CELLS; j=j+4){
+	    double result[4];
+ 	    for (int j=0; j<NUMBER_OF_INPUT_CELLS; j=j+4){
        	    	//input[j] = pixel[j] ? 1 : 0;
 		__m256d v4_pixel = _mm256_set_pd(pixel[j], pixel[j+1], pixel[j+2], pixel[j+3]);
 		__m256d v4_input = _mm256_cmp_pd(v4_pixel, v4_zero, 4);
@@ -186,17 +186,10 @@ void testLayer(Layer *l){
 		__m256d v4_weight = _mm256_set_pd(weight[j], weight[j+1], weight[j+2], weight[j+3]);
 		__m256d v4_mul = _mm256_mul_pd(v4_weight, v4_input);
 		v4_sum = _mm256_add_pd(v4_mul, v4_sum); 
-        //	c_output_test += weight[j] * input[j];
     	    }
 	    _mm256_store_pd(result, v4_sum);
 	    double c_output_test = result[0] + result[1]+result[2]+result[3];
 	
-	//    double temp = 0;
-	  //  for (int j=0; j<NUMBER_OF_INPUT_CELLS; j++){
-//		input[j] = pixel[j]?1:0;
-//		temp += weight[j]*input[j];
-//	    }	
-	    //printf("\ntemp= %f, simd_cout=%f", temp, c_output_test);	
 	    l->cell[i].output = c_output_test/NUMBER_OF_INPUT_CELLS;   
         }
         
